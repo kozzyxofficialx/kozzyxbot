65d5cb99b43fb7d71460a53537294e27a98dc228
Bacsó Balázs <ckazros@Ckazros-MacBook-M4.local>
2026-04-14 23:45:35 +0200

    chore: auto-save changes

--- src/prefixCommands/features/afk.js
+++ src/prefixCommands/features/afk.js
@@ -1,9 +1,12 @@
-import { setAfk } from "../../utils/database.js";
+import { setAfk, getGuildSettings } from "../../utils/database.js";
 import { replyEmbed } from "../../utils/embeds.js";
 
 export default {
     name: "afk",
     async execute(message, args) {
+        if (getGuildSettings(message.guild.id).plugins?.afk === false) {
+            return replyEmbed(message, { type: "error", title: "🚫 Feature Disabled", description: "The AFK system is disabled in this server." });
+        }
         const reason = args.join(" ") || "AFK";
         await setAfk(message.author.id, reason);
         return replyEmbed(message, { type: "afk", title: "😴 AFK Set", description: `You are now AFK: **${reason}**` });
